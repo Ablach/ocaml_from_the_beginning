1c3ee2976a0ac02061c633f153b287615309d66a
elscoto <scottfmora@gmail.com>
2020-07-02 12:32:28 +0100

    ch2 done

---
+++ ch2.ml
@@ -0,0 +1,12 @@
+let times_ten  n = n * 10
+
+let both_non_zero n m = not (n = 0 || m = 0)
+
+let rec n_tri n = if n = 0 then 0 else n + n_tri (n - 1)
+
+let rec power x n = if n = 1 then x else x * power x (n - 1)
+
+let is_consonant c = not (c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+
+let x = 1 in let x = 2 in x + x
+
