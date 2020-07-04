45669f417d61c7f5db3cae1feb6fed330c90174b
elscoto <scottfmora@gmail.com>
2020-07-04 21:55:16 +0100

    ch14 done

--- ch14.ml
+++ ch14.ml
@@ -10,3 +10,16 @@ let rec deconstruct n = if n > 0.
                         then (floor n, n -. floor n)
                         else let x,y = deconstruct (-. n) in
                              (-. x, y)
+
+let star n =
+  let i = int_of_float (floor (n *. 50.)) in
+  let i' = if i = 50 then 49 else i in
+  for x = 0 to i' do print_char ' ' done;
+  print_string "*\n"
+                    
+let plot f start stop step =
+  let count = ref start in
+  while !count <= stop do
+    star (f !count);
+    count := !count +. step
+  done
